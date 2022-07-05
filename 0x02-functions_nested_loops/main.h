99d8b446badbd8b293684285b459fc283a75f48b
IscoDaLuPrincie <fulakaunda@gmail.com>
2022-07-05 04:16:18 -0700

    print _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -10,16 +10,15 @@
  *
  *    *
  *
- *     * Return: On success 1.
+ *     * Return: On success 0.
  *
  *      * On error, -1 is returned, and errno is set appropriately.
  *
  *       */
 
-int _putchar(char c)
+int _putchar()
 
 {
 
-		return (write(1, &c, 1));
-
+		return 0;
 }
